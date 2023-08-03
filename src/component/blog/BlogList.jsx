025361b9a0a5bb783443921b29da0dc448adb874
Dilara Özdemir <dilaraozdmr35@gmail.com>
2023-08-03 20:00:48 +0300

    BlogList

--- src/component/blog/BlogList.jsx
+++ src/component/blog/BlogList.jsx
@@ -1,9 +1,35 @@
 import React, { Component } from 'react'
+import { withTranslation } from 'react-i18next'
+import BlogApi from '../../services/BlogApi.js';
+//CLASS
+ class BlogList extends Component {
+  //componentteki isim
+  static displayName="Blog_List";
 
-export default class BlogList extends Component {
+  constructor(props){
+    super(props);
+
+    //state
+    this.state={
+      blogList:[],
+    }
+    //BIND
+  }
+
+  //CDM
+  componentDidMount(){
+    BlogApi.blogServicesList()
+    .then((response)=>{console.log(response);
+      this.setState({blogList:response.data})
+    }).
+    catch((err)=>{console.log('error:',err)})
+  }
+
   render() {
     return (
       <div>BlogList</div>
     )
   }
 }
+
+export default withTranslation()(BlogList)
