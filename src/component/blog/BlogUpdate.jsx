bd54497bd110db11659e581810eaa10d69dd6d00
Dilara Özdemir <dilaraozdmr35@gmail.com>
2023-08-21 16:58:16 +0300

    update

--- src/component/blog/BlogUpdate.jsx
+++ src/component/blog/BlogUpdate.jsx
@@ -35,17 +35,61 @@ class BlogUpdate extends Component {
   }
 
   //Function
-  onChangeInputValue = (event) => {
+  onChangeInputValue =  (event) => {
     const { name, value } = event.target;
+    const backendErrorHandling = { ...this.state.validationErrors };
+    backendErrorHandling[name] = undefined;
     this.setState({
       [name]: value,
+      backendErrorHandling,
     });
-  }
+  };
+
+  updateSubmit =async(event) => {
+    event.preventDefault();
+    const { header, content } = this.state;
+    const blogDto = {
+      header,
+      content,
+    };
+    console.log(blogDto);
+    try{
+      this.setState({
+        spinnerData: true,
+        multipleRequest: true,
+      });
+
+      //update
+      const response=await BlogApi.blogServiceUpdate(this.state.id,blogDto);
+      if(response.status===200){
+        console.log("güncelleme başarılı");
+        //SPINNER DONDURMEDEN ONCE
+        this.setState({
+          spinnerData: false,
+          multipleRequest: false,
+        });
+        //PHP
+        this.props.history.push("/blog/list");
+      }
+    } catch(err){
+      console.log(err);
+      this.setState({
+        //spinnerData: true,
+        multipleRequest: true,
+      });
+
+      if(err.response.data.validationErrors){
+        this.setState({validationErrors:err.response.data.validationErrors});
+      }
+    }
+  };
 
   static displayName = "Blog_Update";
+
   render() {
-    const {t}=this.props;
-    const {validationErrors } = this.state;
+    const { t } = this.props;
+    const {validationErrors,multipleRequest} = this.state;
+    const { header,content } = validationErrors;
 
     return (
       <React.Fragment>
@@ -60,6 +104,7 @@ class BlogUpdate extends Component {
             placeholder={t("blog_header")}
             required={true}
             onChange={this.onChangeInputValue}
+            value={this.state.header}
           />
           <span className="text-danger">{validationErrors.content}</span>
         </div>
@@ -73,23 +118,22 @@ class BlogUpdate extends Component {
             placeholder={t("blog_content")}
             required={true}
             onChange={this.onChangeInputValue}
+            value={this.state.content}
           />
           <span className="text-danger">{validationErrors.content}</span>
         </div>
 
-        
         <button
-            type="submit"
-            className="btn btn-primary mb-5"
-            disabled={ this.state.multipleRequest}
-            onClick={this.createSubmit}
-          >
-            {this.state.spinnerData && (
-              <span className="spinner-border text-warning"></span>
-            )}{" "}
-            {t("submit")}{" "}
-          </button>
-
+          type="submit"
+          className="btn btn-primary mb-5"
+          disabled={this.state.multipleRequest}
+          onClick={this.updateSubmit}
+        >
+          {this.state.spinnerData && (
+            <span className="spinner-border text-warning"></span>
+          )}{" "}
+          {t("submit")}{" "}
+        </button>
       </React.Fragment>
     );
   }
